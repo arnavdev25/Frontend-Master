bbdd6bd61473cfe5ed66ed1fce5d25ecf8825985
Aman Singh <aman.iit96@gmail.com>
2022-03-29 22:09:18 +0530

    instructor and course image data added

--- scripts/courses.js
+++ scripts/courses.js
@@ -7,10 +7,34 @@ const append = (data, parent)=>{
     data.forEach((el) => {
         // console.log('el:', el)
 
+        let mainDiv = document.createElement('div');
+        mainDiv.id = "mainDiv";
+
         let innerDiv = document.createElement('div');
+        innerDiv.id = "innerDiv";
+        let outerDiv = document.createElement('div');
+        outerDiv.id = "outerDiv";
+
+        let img1 = document.createElement('img');
+        img1.src = el.instructor_img_url;
+        img1.id = "instructor_img";
+
+        let img2 = document.createElement('img');
+        img2.src = el.course_img_url;
+        img2.id = "courses_img";
+
+
+        let name = document.createElement('p');
+        name.innerText = el.instructorName;
+
+        
 
         let title = document.createElement('h1');
         title.innerText = el.title;
+
+
+        let description = document.createElement('p');
+        description.innerText = el.course_description;
 
         let btn1 = document.createElement('button');
         btn1.innerText = "Watch Free Preview";
@@ -24,12 +48,17 @@ const append = (data, parent)=>{
 
 
 
+        outerDiv.append(title,img1,name,description,btn1,btn2)
 
-        innerDiv.append(title,btn1,btn2);
+        innerDiv.append(img2);
 
-        parent.append(innerDiv);
+        mainDiv.append(innerDiv,outerDiv)
+
+        parent.append(mainDiv);
         
     })
 }
 
 export default append;
+
+
