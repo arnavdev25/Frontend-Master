92503de33b47ee7b9634839c669f477a6f516cad
Susmita549 <99960489+Susmita549@users.noreply.github.com>
2022-03-29 23:44:48 +0530

    comp

---
+++ scripts/joinnow.js
@@ -0,0 +1,25 @@
+// let first=document.getElementById("first")
+
+
+function myFunction(){
+    document.getElementById("first").style.backgroundColor="red";
+  
+   
+}
+function myFunction1(){
+   
+    document.getElementById("sec").style.backgroundColor="red";
+    
+   
+}
+function myFunction2(){
+   
+    document.getElementById("third").style.backgroundColor="red";
+   
+   
+}
+function myFunction3(){
+
+    document.getElementById("fourth").style.backgroundColor="red";
+   
+}
