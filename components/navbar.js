1a49229abe8a195f8ef2bebf3fc597ea649d7876
Aniket Solanki <solankianiket0411@gmail.com>
2022-03-29 21:14:02 +0530

    small fix

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,5 @@
-function navbar(){
-    return ` <nav class="navbar">
+function navbar() {
+  return ` <nav class="navbar">
     <img src="https://static.frontendmasters.com/assets/fm/js/static/frontendmasters.e1e10b58c1.svg" id="logo">
        <ul class="nav-menu">
            <li class="nav-item">
@@ -22,7 +22,8 @@ function navbar(){
            <span class="bar"></span>
            <span class="bar"></span>
        </div>
-   </nav>`
+       <svg class="diagonal" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 10" preserveAspectRatio="none"><defs><linearGradient id="linear" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" stop-color="#c02d28"></stop><stop offset="100%" stop-color="#e66225"></stop></linearGradient></defs><polygon points="0,2 0,0 100,0 100,10" fill="url(#linear)"></polygon></svg>
+   </nav>`;
 }
 
 export default navbar;
