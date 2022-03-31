da72248b3ae35058f0f59ed3085f669c6d79bbe7
Muskan Shaw <princesahil0065@gmail.com>
2022-03-31 10:42:07 +0530

    Modify4

--- scripts/icon1.js
+++ scripts/icon1.js
@@ -78,26 +78,3 @@ let main=document.getElementById("main")
 import {display} from "../components/display.js"
 // console.log(navbar)
 display(icon1arr,main)
-const numb = document.querySelector(".numb");
-let counter = 0;
-let id = setInterval(() => {
-  if (counter == 100) {
-    let right_div = document.querySelector(".bar.right");
-    right_div.classList.add("reverse");
-    let left_div = document.querySelector(".bar.left");
-    left_div.classList.add("reverse-left");
-    let innerId = setInterval(() => {
-      if (counter == 0) {
-        clearInterval(innerId);
-      } else {
-        counter -= 1;
-
-        numb.textContent = counter + "%";
-      }
-    }, 20);
-    clearInterval(id);
-  } else {
-    counter += 1;
-    numb.textContent = counter + "%";
-  }
-}, 20);
